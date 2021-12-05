5b6cf0a4e8f82e53df3c476cb9e5da6bb5a4748c
mjh2g3 <mjh2g3@gmail.com>
2021-12-04 19:36:34 -0800

    Updated the scene to be bigger

--- CSS551_FinalProject_RayMichael/Assets/Model/TheWorld2.cs
+++ CSS551_FinalProject_RayMichael/Assets/Model/TheWorld2.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class TheWorld2 : MonoBehaviour
 {
+    public Transform clawPos = null;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Assert(clawPos != null);
     }
 
     // Update is called once per frame
--- CSS551_FinalProject_RayMichael/Assets/UI/MainController.cs
+++ CSS551_FinalProject_RayMichael/Assets/UI/MainController.cs
@@ -5,11 +5,15 @@ using UnityEngine;
 public class MainController : MonoBehaviour
 {
     public Camera mainCamera = null;
+    public TheWorld2 mModel = null;
+
+    private GameObject mSelected;
 
     // Start is called before the first frame update
     void Start()
     {
         Debug.Assert(mainCamera != null);
+        Debug.Assert(mModel != null);
     }
 
     // Update is called once per frame
